bcac492bb5353aec6f5f95c67a36702366c7165c
David Gehrig <gehrig.david@gmail.com>
2022-11-19 07:18:35 +0100

    Set value after scaling is defined. This resolves an issue where scaling was not applied for the first value of a measurement series.

--- smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
+++ smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
@@ -82,12 +82,23 @@ class HdlcDlmsParser:
         if isinstance(self._dlms_data.value, list):
             #pylint: disable=unsubscriptable-object
             parsed_objects = self._client.parsePushObjects(self._dlms_data.value[0])
+           
+            value_objects = {}
             for index, (obj, attr_ind) in enumerate(parsed_objects):
                 if index == 0:
                     # Skip first (meta-data) object
                     continue
+                if attr_ind == 2:
+                    # Temp store and skip values for now, so that scaling is defined before setting the value
+                    value_objects[index] = obj
+                    continue
                 self._client.updateValue(obj, attr_ind, self._dlms_data.value[index])
                 LOGGER.debug("%s %s %s: %s", obj.objectType, obj.logicalName, attr_ind, obj.getValues()[attr_ind - 1])
+
+            for index, value_object in value_objects.items():
+                # Set values
+                self._client.updateValue(value_object,2, self._dlms_data.value[index])
+                LOGGER.debug("%s %s %s: %s", value_object.objectType, value_object.logicalName, 2, value_object.getValues()[1])
         self._dlms_data.clear()
         return {obj.getName(): obj for obj, _ in parsed_objects}
 
